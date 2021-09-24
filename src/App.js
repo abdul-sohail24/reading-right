9aba8ce81b73d513121bf2fe9e519a55b77c050d
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-24 17:09:27 +0530

    Exported search engine function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Load } from "./api";
+import { Search } from "./search";
 import Image from "./image";
+import { getSuggestedQuery } from "@testing-library/dom";
 
 function App() {
+	// Search Data
+	const search = Search();
+	// Load Data
 	const data = Load();
 	console.log(data);
 	return (
 		<>
-			{data.map((image) => (
-				<Image src={image.urls.thumb} />
-			))}
+			<div className="row">
+				<div className="col-8">{search}</div>
+				{/* Load Data */}
+				<div className="col-12">
+					{data.map((image) => (
+						<Image src={image.urls.thumb} />
+					))}
+				</div>
+			</div>
 		</>
 	);
 }
