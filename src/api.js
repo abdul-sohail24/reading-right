e03dad449a808e83b2ca61b2de2a1c1ce7a1ffa0
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-24 16:38:07 +0530

    Solved multiple data fetch issue using useEffect Hook

--- src/api.js
+++ src/api.js
@@ -3,13 +3,16 @@ import axios from "axios";
 
 function Load() {
 	const [state, setState] = useState([]);
-	axios
-		.get(
-			"http://api.unsplash.com/photos?client_id=9qzpPMCI079pQAI2o4KLwKdOD3TO7BLzgdeTG2Ljdb8"
-		)
-		.then((data) => {
-			setState(data.data.urls.thumb);
-		});
+	useEffect(() => {
+		axios
+			.get(
+				"http://api.unsplash.com/photos?client_id=9qzpPMCI079pQAI2o4KLwKdOD3TO7BLzgdeTG2Ljdb8"
+			)
+			.then((data) => {
+				setState(data.data);
+			});
+	}, []);
+
 	return state;
 }
 
