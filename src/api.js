434b086cbdd367c377daf4740b3799c934f3f96b
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-25 13:25:18 +0530

    Added searchimages function

--- src/api.js
+++ src/api.js
@@ -16,4 +16,21 @@ function Load() {
 	return state;
 }
 
-export { Load };
+function SearchImages(query) {
+	const [state, setState] = useState([]);
+	useEffect(() => {
+		axios
+			.get(
+				"http://api.unsplash.com/search/photos?query=" +
+					query +
+					"&client_id=9qzpPMCI079pQAI2o4KLwKdOD3TO7BLzgdeTG2Ljdb8"
+			)
+			.then((data) => {
+				setState(data.data.results);
+			});
+	}, [query]);
+
+	return state;
+}
+
+export { Load, SearchImages };
