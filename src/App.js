162fc703cafc7562e6845313e4f3da32c078b5e2
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-25 13:23:32 +0530

    Added searchquery code to main file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Load } from "./api";
-import { Search } from "./search";
+// import { Search } from "./search";
 import Image from "./image";
-import { getSuggestedQuery } from "@testing-library/dom";
+import { SearchImages } from "./api";
 
 function App() {
 	// Search Data
-	const search = Search();
+	const [query, setQuery] = useState();
+	const [searchQuery, setSearch] = useState();
+	console.log(query);
+	console.log(SearchImages(query));
+	const search = () => {
+		setSearch(query);
+	};
+
+	const searchData = SearchImages(query);
+	console.log(searchData);
 	// Load Data
 	const data = Load();
-	console.log(data);
+	// console.log(data);
 	return (
 		<>
+			<input type="text" onChange={(e) => setQuery(e.target.value)} />
+			<button type="button" onClick={search} className="btn btn-danger">
+				Search
+			</button>
 			<div className="row">
 				<div className="col-8">{search}</div>
 				{/* Load Data */}
+				{/* <div className="col-12">
+					{data.map((image, key) => (
+						<Image src={image.urls.thumb} key={key} />
+					))}
+				</div> */}
+				{/* Display Data */}
 				<div className="col-12">
-					{data.map((image) => (
-						<Image src={image.urls.thumb} />
+					{searchData.map((image, key) => (
+						<Image src={image.urls.thumb} key={key} />
 					))}
 				</div>
 			</div>
